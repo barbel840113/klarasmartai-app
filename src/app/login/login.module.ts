e6fe64e65af0175869c5f5b7e09cdd85823c02cc
Michal Sivak <sivakmichal@gmail.com>
2018-09-24 23:11:04 +1000

    Add

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -6,25 +6,37 @@ import { LoginComponent } from './login/login.component';
 import { AppDashboardComponent } from '../app-dashboard/app-dashboard.component';
 import { MatGridListModule, MatIconModule, MatMenuModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { LayoutModule } from '@angular/cdk/layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDialog} from '@angular/material'
+import {FormsModule} from '@angular/forms';
+import {MAT_DIALOG_DATA, MAT_DIALOG_DEFAULT_OPTIONS} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     LoginRoutingModule,
+   // MatDialog,
     MatCardModule,
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
     MatIconModule,
+    FormsModule,
     MatButtonModule,
     LayoutModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
   ],
-  declarations: [LoginComponent, AppDashboardComponent]
+  declarations: [LoginComponent, AppDashboardComponent],
+  providers: [
+    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}}
+  ]
 })
 export class LoginModule { }
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointState, BreakpointObserver } from '@angular/cdk/layout';
 
-export interface Tile{
+export interface Tile {
   color: string;
   cols: number;
   rows: number;
- 
+
 }
 
 @Component({
@@ -15,10 +16,39 @@ export interface Tile{
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
-  
-  public tiles: Tile[]= [  
-    { cols: 2, rows: 2, color: 'lightpink'},  
+  /**
+   * 
+   */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Card 1', cols: 1, rows: 1 },
+        ];
+      }
+      return [
+        { title: 'Card 1', cols: 2, rows: 1 },
+      ];
+    })
+  );
+
+  /**
+   * 
+   */
+  username: string;
+
+  /**
+   * Password
+   */
+  password: string;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+
+  }
+
+  public tiles: Tile[] = [
+    { cols: 2, rows: 2, color: 'lightpink' },
   ];
 
   ngOnInit() {
